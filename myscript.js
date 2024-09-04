1258a91b2678e2ce2176d48830b1fb021a50f557
SebastianFabio <seabass.fabio@gmail.com>
2024-09-04 13:51:09 -0700

    update files

--- myscript.js
+++ myscript.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-	
-  
+	$(".True").click(function(){
+		$(this).css({"color": "rgb(69, 222, 27)", "font-size": "28px"});
+	});
+	$(".False").click(function(){
+		$(this).css({"color": "rgb(222, 27, 27)", "font-size": "28px"});
+	});
 });
